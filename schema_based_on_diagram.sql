bea729fe68c3acd22e03bacc20f25e8e43a116d8
Javier Vargas <Javieralejandroortga@gmail.com>
2023-08-22 11:32:30 -0400

    Implement tables from diagram

--- schema_based_on_diagram.sql
+++ schema_based_on_diagram.sql
@@ -23,3 +23,28 @@ CREATE TABLE invoices {
   PRIMARY KEY(id),
   FOREIGN KEY(medical_history_id) REFERENCES medical_histories(id)
 };
+
+CREATE TABLE invoice_items {
+  id int GENERATED ALWAYS AS IDENTITY,
+  unit_price decimal,
+  quantity int,
+  total_price decimal,
+  invoice_id int,
+  treatment_id int
+  PRIMARY KEY(id),
+  FOREIGN KEY(treatment_id) REFERENCES treatments(id),
+  FOREIGN KEY(invoice_id) REFERENCES invoices(id)
+};
+CREATE TABLE treatments {
+  id int GENERATED ALWAYS AS IDENTITY,
+  type varchar,
+  name varchar
+  PRIMARY KEY(id)
+};
+CREATE TABLE medical_treatments {
+  medical_histories_id int,
+  treatments_id int,
+  PRIMARY KEY(medical_histories_id, treatments_id),
+  FOREIGN KEY(medical_histories_id) REFERENCES medical_histories(id)
+  FOREIGN KEY(treatments_id) REFERENCES treatments(id)
+};
